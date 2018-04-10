509543dbd4b2f91100bd21ee21852c3f1ef1b825
omar.bakry <omar.bakry@almoasher.net>
2018-04-10 15:58:36 +0200

    asd

--- js/services.js
+++ js/services.js
@@ -5,7 +5,7 @@ if(servicesData){
     servicesData=JSON.parse(servicesData);
     html="";
     servicesData.forEach(function(item){
-        html+='<li><a href="javascript:void(0)"><span style="display: inherit;"><img src="'+item.image+'"></span>'+item.name+'</a><span class="border-shape"></span></li>';
+        html+='<li><a href="javascript:void(0)"><span><i class="fullscr-nav-icon fas '+item.icon+'"></i></span>'+item.name+'</a><span class="border-shape"></span></li>';
     });
     $(".fullscr-navigation.position-initial ul.list-unstyled").html(html);
 }
